4c44b81512b4a566df9c91394f1f9fd733966502
Josua Mayer <josua@solid-run.com>
2026-09-09 11:10:42 +0200

    disable i2c2 pins pull-up to avoid io voltage conflict with cpu
    The IO voltage used on aiot board can differ between SoMs and is
    selected by DIP switch for most components on i2c bus.
    
    The MCU however operates from a fixed 3.3V supply. This can lead to
    voltage level conflicts when mcu pins pull up to 3.3V while other
    components on the bus use a different voltage (e.g. 1.8V).
    
    Disable pull-ups on i2c2 pins, the MCU is acting as a slave which
    generally can expect and rely on external pull-ups on the bus.
    
    Set gpio high-speed mode as appropriate for i2c.

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -83,7 +83,7 @@ void MX_I2C2_Init(void)
 
   /* USER CODE END I2C2_Init 1 */
   hi2c2.Instance = I2C2;
-  hi2c2.Init.Timing = 0x00303D5D;
+  hi2c2.Init.Timing = 0x00303D5B;
   hi2c2.Init.OwnAddress1 = 48;
   hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
@@ -98,7 +98,7 @@ void MX_I2C2_Init(void)
 
   /** Configure Analogue filter
   */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_DISABLE) != HAL_OK)
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
@@ -209,8 +209,8 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
     */
     GPIO_InitStruct.Pin = i2c2_SCL_Pin|i2c2_SDA_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF5_I2C2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
